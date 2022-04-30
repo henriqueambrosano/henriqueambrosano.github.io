63140c76c547345767a8d1af43d4ffafaa4d1d62
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-04-30 18:31:06 -0300

    site finalizado -adicionado typed.js para animacao

--- script.js
+++ script.js
@@ -5,11 +5,47 @@ $(document).ready(() => {
     } else {
       $('.navbar').removeClass('sticky');
     }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
   });
+
+  // slide-up script
+  $('.scroll-up-btn').click(() => {
+    $('html').animate({ scrollTop: 0 });
+  });
+
   //   toggle menu/navbar Script
   $('.menu-btn').click(() => {
     $('.navbar .menu').toggleClass('active');
     $('.menu-btn i').toggleClass('active');
+  });
+
+  // typing animation script using typed.js
+  const typed = new Typed('.typing', {
+    strings: [
+      'Front-end Developer',
+      'Back-end Developer',
+      'Freelancer',
+      'Programer',
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  const typed2 = new Typed('.typing-2', {
+    strings: [
+      'Front-end Developer',
+      'Back-end Developer',
+      'Freelancer',
+      'Programer',
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
   });
 
   // owl carousel script
