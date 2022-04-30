814af404574bb8b2782888be6537229d15c7bf58
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-04-30 15:53:38 -0300

    teams section e owlCarousel

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ $(document).ready(() => {
     $('.navbar .menu').toggleClass('active');
     $('.menu-btn i').toggleClass('active');
   });
+
+  // owl carousel script
+  $('.owl-carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    stagePadding: 50,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
